5fb8323dc299620e343759722d9a6737675a1547
Helene1219 <helene.wlache@wanadoo.fr>
2024-04-08 23:16:47 +0200

    test-api

---
+++ test_API.py
@@ -0,0 +1,29 @@
+from api import get_ids
+from api import identite_client
+from api import load_prediction
+from api import shap_value
+
+
+#import pandas as pd
+#import logging
+
+#logging.basicConfig(filename='test.log', level=logging.DEBUG) #, format='%(asctime)s - %(message)s', datefmt='%d-%b-%y %H:%M:%S' )
+
+def test_get_ids():
+    ids = get_ids()
+    print(f'le dataset contient {len(ids.values())} ids')
+    assert len(ids.values()) >= 1
+
+
+def test_client_details(id=100002.0):
+    data_client = identite_client(id)
+    assert 'AMT_INCOME_TOTAL' in data_client.columns
+    
+def test_client_prediction(id=100002.0):
+    prediction,statut = load_prediction(id)
+    assert prediction>0
+
+    
+def test_shap_value(id=100002.0):
+    shap_id=shap_value(id)
+    assert len(shap_id)>0
