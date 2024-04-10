1b4177c60e871c1a8822b10ecc63fcb7006929d7
AlexandreGazagnes <a_syoez@yahoo.fr>
2024-04-10 14:19:48 +0200

    update test

---
+++ tests/test_API.py
@@ -0,0 +1,31 @@
+from api import (get_ids, identite_client, load_prediction, nb_credits,
+                 shap_value)
+
+#import pandas as pd
+#import logging
+
+#logging.basicConfig(filename='test.log', level=logging.DEBUG) #, format='%(asctime)s - %(message)s', datefmt='%d-%b-%y %H:%M:%S' )
+
+def test_get_ids():
+    ids = get_ids()
+    print(f'le dataset contient {len(ids.values())} ids')
+    assert len(ids.values()) >= 1
+
+
+def test_client_details(id=100002.0):
+    data_client = identite_client(id)
+    assert 'CODE_GENDER' in data_client.columns
+    
+def test_client_prediction(id=100002.0):
+    prediction,statut = load_prediction(id)
+    assert prediction>0
+
+    
+def test_shap_value(id=100002.0):
+    shap_id=shap_value(id)
+    assert len(shap_id)>0
+    
+    
+def test_nb():
+    nb=nb_credits()
+    assert nb>0
